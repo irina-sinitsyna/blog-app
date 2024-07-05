b38b9c55a4e95412aca2e4ec1943463937376f8c
irina <iryna@theviewpro.com>
2024-07-05 15:05:37 +0300

    fix: fixed docker configuration for correct work with .env

--- backend/src/auth/jwt.constants.ts
+++ backend/src/auth/jwt.constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: 'jwtkey',
+  secret: process.env.JWT_SECRET || 'jwtkey',
 };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
